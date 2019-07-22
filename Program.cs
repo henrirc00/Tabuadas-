b7f5c3910d695d386ac3224a5e7254f8eca4c209
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-07-22 09:53:03 -0300

    Tabuadas

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tabuadas
+{
+    class Program
+    {
+        static void Main(string[] args){
+        
+
+           for(int y = 2; y <= 10; y++){
+             for(int x = 0; x <= 10; x++){
+                Console.WriteLine(y+ " X "+x+" = "+y*x);
+                
+
+             }
+             Console.WriteLine();
+             Console.WriteLine("-------------------------");
+             Console.WriteLine();
+            }
+        }    
+           
+    }  
+    
+        
+    
+}
